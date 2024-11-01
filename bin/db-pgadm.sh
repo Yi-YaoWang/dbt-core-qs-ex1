33f05aa180e70e7b54ee84515f11671fe2b12ef0
Justin Wang <justin17728@gmail.com>
2024-11-01 14:00:01 +0800

    Update Shell for MacOS

--- bin/db-pgadm.sh
+++ bin/db-pgadm.sh
@@ -1,2 +1,2 @@
-#!//bin/zsh
+#!/bin/bash
 podman run --pod postgre-sql -e 'PGADMIN_DEFAULT_EMAIL=admin@cloudspinx.com' -e 'PGADMIN_DEFAULT_PASSWORD=StrongPassw0rd' --name pgadmin  -d docker.io/dpage/pgadmin4:latest
